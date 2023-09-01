f50942dedb8db604081458cdef4e6a853d61e234
syun031 <140134707+syun031@users.noreply.github.com>
2023-09-01 20:14:16 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,12 @@ class ItemsController < ApplicationController
 #def index
 #end
 
+private
+
+def message_params
+  params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+end
+
 #def move_to_index
   #unless user_signed_in?
     #redirect_to action: :index
