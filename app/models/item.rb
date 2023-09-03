bb3e394f706b0deb22f7ea433740eef4411a1d97
syun031 <140134707+syun031@users.noreply.github.com>
2023-09-03 20:32:23 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,17 @@
 class Item < ApplicationRecord
   has_one_attached :purchase
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_description
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+  validates :title, :text, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :product_condition_id, numericality:  { other_than: 1 , message: "can't be blank"}
+  validates :shipping_charge_id, numericality:  { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality:  { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id, numericality:  { other_than: 1 , message: "can't be blank"}
 end
