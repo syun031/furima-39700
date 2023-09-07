7be921a0de6d0c118c5acee0bcbc0370351aa245
syun031 <140134707+syun031@users.noreply.github.com>
2023-09-07 18:55:57 +0900

    indexアクションへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'items#index'
   get 'items/new', to: 'items#new', as: 'new_item'
+  get 'items', to: 'items#index', as: 'items'
   resources :items
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
