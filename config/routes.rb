2ed953ec88710211d3138deca673e5e764755bdf
syun031 <140134707+syun031@users.noreply.github.com>
2023-09-15 14:16:26 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root to: 'items#index'
   get 'items/new', to: 'items#new', as: 'new_item'
   get 'items', to: 'items#index', as: 'items'
-  resources :items, except: [:edit, :update]
+  resources :items, except: [:edit, :update, :destroy]
   get 'items/:id/edit', to: 'items#edit', as: 'edit_item'  
   patch 'items/:id', to: 'items#update'
+  delete 'items/:id', to: 'items#destroy'
   resources :items
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
