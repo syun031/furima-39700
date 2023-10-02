4a108b5096873ced160a3d5ed20d66b87d04bf94
syun031 <140134707+syun031@users.noreply.github.com>
2023-10-02 17:52:32 +0900

    修正

---
+++ app/javascript/item_price.js
@@ -0,0 +1,40 @@
+document.addEventListener("turbo:load", () => {
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+
+  // 価格入力フィールドの値が変更されたときに呼び出す関数
+  function updatePriceInfo() {
+    const inputValue = priceInput.value;
+    if (inputValue !== "") {
+      const price = parseFloat(inputValue);
+
+      // 販売手数料と利益の計算
+      let tax, profit;
+
+      if (price < 1000) {
+        // 価格が1000未満の場合、手数料0円
+        tax = 0;
+        profit = price;
+      } else {
+        // 価格が1000以上の場合、10%の手数料を計算
+        tax = Math.floor(price * 0.1);
+        profit = price - tax;
+      }
+
+      // HTML要素に結果を表示
+      addTaxDom.textContent = `¥${tax.toLocaleString()}`;
+      profitDom.textContent = `¥${profit.toLocaleString()}`;
+    } else {
+      // 入力が空の場合でも手数料と利益を空欄で表示
+      addTaxDom.textContent = `¥`;
+      profitDom.textContent = `¥`;
+    }
+  }
+
+  // 価格入力フィールドの値が変更されたときに呼び出すイベントリスナーを設定
+  priceInput.addEventListener("input", updatePriceInfo);
+
+  // ページロード時に初期表示として手数料と利益を計算
+  updatePriceInfo();
+});
