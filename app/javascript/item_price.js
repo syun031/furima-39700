7a1901d62697b6efbcfab052370ccfbd0aa86775
syun031 <140134707+syun031@users.noreply.github.com>
2023-10-03 19:58:06 +0900

    あ

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -26,41 +26,15 @@ document.addEventListener("turbo:load", () => {
       addTaxDom.textContent = `¥${tax.toLocaleString()}`;
       profitDom.textContent = `¥${profit.toLocaleString()}`;
     } else {
-      // 入力が空の場合でも手数料と利益を空欄で表示
-      addTaxDom.textContent = `¥`;
-      profitDom.textContent = `¥`;
+      // 値が空の場合も手数料と利益をクリア
+      addTaxDom.textContent = "";
+      profitDom.textContent = "";
     }
   }
 
   // 価格入力フィールドの値が変更されたときに呼び出すイベントリスナーを設定
   priceInput.addEventListener("input", updatePriceInfo);
 
-  // ページロード時に初期表示として手数料と利益を計算
+  // ページロード時にも手数料と利益を更新
   updatePriceInfo();
-});
-=======
-window.addEventListener("turbo:load", () => {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    if (inputValue !== "") {
-      // 販売手数料の計算
-      const tax = Math.floor(inputValue * 0.1); // 10%の手数料と仮定
-
-      // 利益の計算
-      const profit = inputValue - tax;
-
-      // HTML要素に結果を表示
-      const addTaxDom = document.getElementById("add-tax-price");
-      addTaxDom.innerHTML = `¥${tax.toLocaleString()}`; // 手数料を通貨形式で表示
-      const profitDom = document.getElementById("profit");
-      profitDom.innerHTML = `¥${profit.toLocaleString()}`; // 利益を通貨形式で表示
-    } else {
-      // 入力が空の場合、結果をクリア
-      const addTaxDom = document.getElementById("add-tax-price");
-      addTaxDom.innerHTML = "";
-      const profitDom = document.getElementById("profit");
-      profitDom.innerHTML = "";
-    }
-  });
 });
